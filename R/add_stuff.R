c26036e75e996cd8808f9d2fa235ccf64bc0dad9
jalapic <jc3181@columbia.edu>
2018-12-05 09:26:06 -0600

    add add_stuff

---
+++ R/add_stuff.R
@@ -0,0 +1,13 @@
+#'  Add stuff
+#'  @param a A number
+#'  @param b Another number
+#'  @return Yet another number
+#'  @examples
+#'      add_stuff(3,2)
+#'  @export
+
+
+add_stuff <- function(a,b){
+  out <- a+b
+  return(out)
+}
